9a1c8dcb8c20329f0c5d01f123aaca2d503bad27
Sumin Ha <minha62@ewhain.net>
2023-10-05 05:02:09 +0900

    Remove ayncio

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from filtering import Filtering
 from product_list import ProductList
 from simple_detail import SimpleDetail
 from magazines import Magazine
-import asyncio
 
 class Item(BaseModel):
     userNeed: str
@@ -27,5 +26,5 @@ async def filtering_prompt(item: Item):
 
 @app.post("/items/details")
 async def details_prompt(item: Url):
-    simple_detail = asyncio.run(SimpleDetail(item.productUrl))
+    simple_detail = await SimpleDetail(item.productUrl)
     return simple_detail
