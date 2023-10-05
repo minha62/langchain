ba7657a583ada1e806d7b86e7094d154fbb23bcf
Sumin Ha <minha62@ewhain.net>
2023-10-05 14:37:02 +0900

    Add subprocess code

--- product_detail.py
+++ product_detail.py
@@ -2,7 +2,7 @@ import subprocess
 from playwright.async_api import async_playwright
 
 subprocess.run(["playwright", "install"])
-subprocess.run(["playwright", "install-deps"])
+subprocess.run(["sudo", "playwright", "install-deps"])
 
 async def ProductDetails(url):
     async def reviewObject(page, reviewElement):
