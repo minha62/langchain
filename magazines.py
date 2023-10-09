b5b4234aed151cc209947cdf7961fa5ae98e6fff
Sumin Ha <minha62@ewhain.net>
2023-10-10 06:59:59 +0900

    Make prompts shorter

--- magazines.py
+++ magazines.py
@@ -9,36 +9,12 @@ def Magazine(user_input):
     os.environ.get('OPENAI_API_KEY')
     llm = OpenAI(temperature=0.9)
 
-    template = """You are the helpful agent that creates url that matches the user's input.
-    - output should be only one URL. do not add any description about output.
-
-    If there is something related to shopping keywords, you have to add "?q=" and the component like this.
-    You don't have to care about categories of clothes like bags, tops, skirts, jeans and so on.
-
-    Do not add another filter or component like sorting, price and so on.
-
-    Example 1)
-    user input: 키치한 옷 찾아줘
-    URL: https://www.musinsa.com/search/musinsa/magazine?q=키치
-
-    Example 2)
-    user input: 연예인이 착용한 가방 보여줘
-    URL: https://www.musinsa.com/search/musinsa/magazine?q=연예인착용
-
-    Example 3)
-    user input: 힙한 옷 보여줘
-    URL: https://www.musinsa.com/search/musinsa/magazine?q=힙한
-
-    Example 4)
-    user input: y2k 패션 추천해줘
-    URL: https://www.musinsa.com/search/musinsa/magazine?q=y2k
-
-    Relavant Information:
-    {history}
-
-    Conversation:
-    user input: {input}
-    URL:"""
+    template = """You are the helpful agent that creates url that matches the user's input. output should be only one URL. do not add any description about output. If there is something related to shopping keywords, you have to add "?q=" and the component like this. You don't have to care about categories of clothes like bags, tops, skirts, jeans and so on. Do not add another filter or component like sorting, price and so on.
+    Example 1) user input: 키치한 옷 찾아줘, URL: https://www.musinsa.com/search/musinsa/magazine?q=키치
+    Example 2) user input: 연예인이 착용한 가방 보여줘, URL: https://www.musinsa.com/search/musinsa/magazine?q=연예인착용
+    Example 3) user input: 힙한 옷 보여줘, URL: https://www.musinsa.com/search/musinsa/magazine?q=힙한
+    Example 4) user input: y2k 패션 추천해줘, URL: https://www.musinsa.com/search/musinsa/magazine?q=y2k
+    Relavant Information: {history} Conversation: user input: {input} URL:"""
 
     prompt = PromptTemplate(
         input_variables=["history", "input"],
--- simple_detail.py
+++ simple_detail.py
@@ -20,26 +20,11 @@ def SimpleDetail(url):
 
     llm = OpenAI(temperature=0.9)
 
-    template = """You are the helpful agent that summarizes product information from input contetns.
-    - You should show size options. Don't add specific length(cm) for size.
-    - You should let customers know the date of delivery arrival
-    - You should show the summary of the review
-
+    template = """You are the helpful agent that summarizes product information from input contents. You should show size options. Don't add specific length(cm) for size. You should let customers know the date of delivery arrival. You should show the summary of the review.
     The result should be like below:
-    Example1) 
-    사이즈 옵션: S, M, L
-    배송 정보: 10/7(토) 도착 예정
-    가장 유용한 리뷰: 164cm, 49kg 여성은 M 사이즈를 구매했으며, "적당한 오버핏으로 길이도 제 키에 딱 맞아서 편하고 예뻐요! 후드티 핏이 아주 굿입니다." 라고 리뷰를 남겼습니다.
-
-    Example2)
-    사이즈 옵션: M, L
-    배송 정보: 10/6(금) 도착 예정
-    가장 유용한 리뷰: 162cm, 53kg 여성은 L 사이즈를 구매했으며, "품과 길이가 모두 만족스럽다"고 리뷰를 남겼습니다.
-
-    Relevant information: {history}
-
-    input contents: {input}
-    result:"""
+    Example1)사이즈 옵션은 총 3가지로 S, M, L이 있습니다. 상품은 10/7(토) 도착 예정입니다. 가장 유용한 리뷰: 164cm/49kg 여성이 M 사이즈를 구매했으며, 적당한 오버핏으로 길이도 키에 딱 맞아서 편하고 예쁘고 후드티 핏이 좋다고 리뷰를 남겼습니다.
+    Example2)사이즈 옵션은 총 2가지로 M, L이 있습니다. 상품은 10/6(금) 도착 예정입니다. 가장 유용한 리뷰: 162cm/53kg 여성이 L 사이즈를 구매했으며, 품과 길이가 모두 만족스럽다고 리뷰를 남겼습니다.
+    Relevant information: {history} input contents: {input} result:"""
 
 
     prompt = PromptTemplate(
