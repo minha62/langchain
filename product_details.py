8853df3d33f22f5dd6bc068723e4ccfb2d88b106
Sumin Ha <minha62@ewhain.net>
2023-10-10 20:05:35 +0900

    Change parameter in gpt()

--- product_details.py
+++ product_details.py
@@ -53,15 +53,15 @@ def Details(apikey, url):
         size_reco_template = """You are the helpful agent that recommends height and weight per size options from input size contents. Calculate average range of heights and weights per sizes and sex. If there is no data in one size option, don't print height and weight but "데이터 없음".
     The result should be like this: Example1)[S size] (여성)155~160cm/45~50kg, (남성)데이터 없음\n[M size] (여성)160~165cm/50~60kg, (남성)170~175cm/64~77kg\n[L size] (여성)165~170cm/60~70kg, (남성)175~180cm/77~100kg Example2)[FREE size] (여성)160~168cm/48~63kg, (남성)172~185cm/62~84kg
     Relevant information:{history} size contents:{input} result:"""
-        sizeReco = "구매자 통계를 기반으로 사이즈를 추천드리겠습니다.\n" + gpt(apikey, str(size_reco), size_reco_template)
+        sizeReco = "구매자 통계를 기반으로 사이즈를 추천드리겠습니다.\n" + gpt(str(size_reco), size_reco_template)
     else:
         sizeReco = "아직 구매자가 없어 추천이 어렵습니다." # 추후 사이즈표 제시
 
     if up_reviews:
         review_sum_template = """You are the helpful agent that summarize reviews. The output has to be 2~3 sentences in Korean. Relevant information:{history} review contents:{input} result:"""
         #  The result should be like this: Example)제품의 품이 넉넉하고 디자인은 이쁘지만 물때가 있어서 검수가 필요해 보인다는 의견이 있었습니다. 핏이 널널하고 예쁘다고 말하는 고객도 있지만, 핏이 아방해서 마음에 들지 않는다는 고객도 있습니다. Example2)시보리가 짱짱해서 마음에 들고, 블랙과 핑크 조합이 귀엽고, 예쁜 디자인과 색상으로 인기가 많다는 평이 있습니다. 품질과 핏이 좋지만 목부분이 살짝 감긴다는 의견이 있습니다.
-        up_sum = gpt(apikey, str(up_reviews[1:]), review_sum_template)
-        worst_sum = gpt(apikey, str(worst_reviews), review_sum_template)
+        up_sum = gpt(str(up_reviews[1:]), review_sum_template)
+        worst_sum = gpt(str(worst_reviews), review_sum_template)
         reviewSum = "구매자들의 리뷰를 유용한 리뷰와 평점이 낮은 리뷰로 나눠서 요약해드리겠습니다.\n" + "[유용한 리뷰] " + up_sum + "\n[낮은 평점 리뷰] " + worst_sum
     else:
         reviewSum = "리뷰가 존재하지 않습니다."
