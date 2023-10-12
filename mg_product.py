341475dc59b32ae330f057563db6d0866a0413ad
Sumin Ha <minha62@ewhain.net>
2023-10-12 13:29:08 +0900

    Do not add list when img url 404

--- mg_product.py
+++ mg_product.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-from urllib.request import Request, urlopen
 
 def MgProducts(mg_search_url):
     #step1 : Download the webpage
-    url= mg_search_url
-    response = requests.get(url)
+    response = requests.get(mg_search_url)
     html_content = response.text
 
     #step2 : Parse the HTML
@@ -14,17 +11,18 @@ def MgProducts(mg_search_url):
 
     mg_list = soup.find_all('li', class_='listItem')
     mg_urls = []
-    # Limit the loop to the first 10 items
-    for unit in mg_list[:10]:
+    # Limit the loop to the first 20 items
+    for unit in mg_list[:20]:
         # Get product URL
         mg_url = unit.find('div', class_='articleInfo').a.get('href')
-        mg_urls.append(mg_url)
+        if "magazine" in mg_url or "news" in mg_url:
+            mg_urls.append(mg_url)
 
     result = True
     mg_clothes = []
     #step1 : Download the webpage
     for i in range(len(mg_urls)):
-        url = mg_urls[i]
+        url = mg_urls[i]     
         response = requests.get(url)
         html_content = response.text
 
@@ -34,57 +32,47 @@ def MgProducts(mg_search_url):
         #step3 : 관련상품 url 가져오기
         goods_units = soup.find_all('li', class_='goods-unit')
 
-        if goods_units and len(mg_clothes) < 6:  # Check if the list is not empty
-            unit = goods_units[0]  # Get the first product
+        if goods_units and len(mg_clothes) < 30:  # Check if the list is not empty
+            for unit in goods_units[:5]:
+                # Get goods number
+                goods_no = unit.get('goods_no')
 
-            # Get goods number
-            goods_no = unit.get('goods_no')
+                # Get product brand
+                product_brand = unit.find('a', class_='brand').get_text()
 
-            # Get product brand
-            product_brand = unit.find('a', class_='brand').get_text()
+                info = unit.find('a', class_='name')
 
-            info = unit.find('a', class_='name')
+                # Get product name
+                product_name = info.get_text()
 
-            # Get product name
-            product_name = info.get_text()
+                # Get product URL
+                product_url = info.get('href')
 
-            # Get product URL
-            product_url = info.get('href')
+                img_url = unit.find('img').get('data-src')
+                if "https:" not in img_url:
+                    img_url = "https:" + img_url
+                if requests.get(img_url).status_code != 200:
+                    # print("error", img_url)
+                    continue
 
-            # Move to product url
-            url = Request(product_url, headers={'User-Agent':'Mozilla/5.0'})
-            html = urlopen(url)
-            soup = BeautifulSoup(html, 'html.parser')
-            
-            # Get image URL
-            meta_tag_img = soup.find('meta', property='og:image')
-            if meta_tag_img:
-                img_url = meta_tag_img.get('content')
-            else:
-                continue
-            # img_url = unit.find('img').get('data-src')
-            # img_url = get_imgUrl(product_url)
+                price = unit.find('span', class_='price').get_text()
 
-            # Get product price
-            meta_tag_price = soup.find('meta', property='og:description')
-            if meta_tag_img:
-                description = meta_tag_price['content']
-                price = re.search(r'(\d{1,3}(?:,\d{3})+)', description).group(1)+'원'
-            else:
-                continue
-            # price = unit.find('span', class_='price').get_text()
-
-            info = {
-                "no": goods_no,
-                "name": product_name,
-                "brand": product_brand,
-                "price" : price,
-                "img": img_url,
-                "url" : product_url,
-            }
-            mg_clothes.append(info)
+                info = {
+                    "no": goods_no,
+                    "name": product_name,
+                    "brand": product_brand,
+                    "price" : price,
+                    "img": img_url,
+                    "url" : product_url,
+                }
+                mg_clothes.append(info)
 
     if len(mg_clothes)==0:
         result = False
 
     return {"result": result, "clothes": mg_clothes}
+
+
+url = "https://www.musinsa.com/search/musinsa/magazine?q=y2k"
+print(MgProducts(url))
+# MgProducts(url)
---
+++ mg_product_orig.py
@@ -0,0 +1,90 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from urllib.request import Request, urlopen
+
+def MgProducts(mg_search_url):
+    #step1 : Download the webpage
+    url= mg_search_url
+    response = requests.get(url)
+    html_content = response.text
+
+    #step2 : Parse the HTML
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    mg_list = soup.find_all('li', class_='listItem')
+    mg_urls = []
+    # Limit the loop to the first 10 items
+    for unit in mg_list[:10]:
+        # Get product URL
+        mg_url = unit.find('div', class_='articleInfo').a.get('href')
+        mg_urls.append(mg_url)
+
+    result = True
+    mg_clothes = []
+    #step1 : Download the webpage
+    for i in range(len(mg_urls)):
+        url = mg_urls[i]
+        response = requests.get(url)
+        html_content = response.text
+
+        #step2 : Parse the HTML
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        #step3 : 관련상품 url 가져오기
+        goods_units = soup.find_all('li', class_='goods-unit')
+
+        if goods_units and len(mg_clothes) < 6:  # Check if the list is not empty
+            unit = goods_units[0]  # Get the first product
+
+            # Get goods number
+            goods_no = unit.get('goods_no')
+
+            # Get product brand
+            product_brand = unit.find('a', class_='brand').get_text()
+
+            info = unit.find('a', class_='name')
+
+            # Get product name
+            product_name = info.get_text()
+
+            # Get product URL
+            product_url = info.get('href')
+
+            # Move to product url
+            url = Request(product_url, headers={'User-Agent':'Mozilla/5.0'})
+            html = urlopen(url)
+            soup = BeautifulSoup(html, 'html.parser')
+            
+            # Get image URL
+            meta_tag_img = soup.find('meta', property='og:image')
+            if meta_tag_img:
+                img_url = meta_tag_img.get('content')
+            else:
+                continue
+            # img_url = unit.find('img').get('data-src')
+            # img_url = get_imgUrl(product_url)
+
+            # Get product price
+            meta_tag_price = soup.find('meta', property='og:description')
+            if meta_tag_img:
+                description = meta_tag_price['content']
+                price = re.search(r'(\d{1,3}(?:,\d{3})+)', description).group(1)+'원'
+            else:
+                continue
+            # price = unit.find('span', class_='price').get_text()
+
+            info = {
+                "no": goods_no,
+                "name": product_name,
+                "brand": product_brand,
+                "price" : price,
+                "img": img_url,
+                "url" : product_url,
+            }
+            mg_clothes.append(info)
+
+    if len(mg_clothes)==0:
+        result = False
+
+    return {"result": result, "clothes": mg_clothes}
